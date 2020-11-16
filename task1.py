55a84754e967cb4c39ea7f87bf8b0617716aec20
MayaDesrochers <mayad459@deltalearns.ca>
2020-11-16 11:31:51 -0800

    update

--- task1.py
+++ task1.py
@@ -6,3 +6,74 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+
+import tkinter as tk 
+from tkinter import *
+
+window = tk.Tk()
+window.title("Madlib Creator")
+nf=Frame()
+
+eoutput1 = StringVar()
+eoutput2 = StringVar()
+eoutput3 = StringVar()
+
+
+def clickFunction():
+    person = e1.get()
+    noun = e2.get()
+    event = e3.get()
+   
+    
+    entry1.delete(0,END)
+    entry1.insert(0,person)
+    entry2.delete(0,END)
+    entry2.insert(0,noun)
+    entry3.delete(0,END)
+    entry3.insert(0,event)
+  
+
+
+l1 = Label(window, text="(person)")
+e1 = Entry(window, width=10)
+l2 = Label(window, text="is too cool for (noun)")
+e2 = Entry(window, width=10)
+l4 = Label(window, text="class.")
+l5 = Label(window, text="Instead, she/he will be attending the (an event)")
+e3 = Entry(window, width=10)
+
+label1=Label(window, text="is too cool for")
+label2=Label(window, text="class.")
+label3=Label(window, text="Instead, she/he will be attending the")
+entry1 = Entry(window, width=10, textvariable=eoutput1)
+entry2 = Entry(window, width=10, textvariable=eoutput2)
+entry3 = Entry(window, width=10, textvariable=eoutput3)
+
+b1 = Button(window, text="Click to create madlib", command=clickFunction)
+
+
+
+l1.grid(row=1,column=1)
+e1.grid(row=1,column=2)
+l2.grid(row=1,column=3)
+e2.grid(row=1, column=4)
+l4.grid(row=1, column=5)
+l5.grid(row=2, column=1)
+e3.grid(row=2, column=2)
+b1.grid(row=3, column=2)
+
+entry1.grid(row=4, column=1)
+label1.grid(row=4, column=2)
+entry2.grid(row=4, column=3)
+label2.grid(row=4, column=4)
+label3.grid(row=5, column=1)
+entry3.grid(row=5, column=2)
+
+
+
+
+mainloop()
+
+
+
+
--- task2.py
+++ task2.py
@@ -12,3 +12,71 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+import math 
+import tkinter as tk 
+from tkinter import *
+
+window = tk.Tk()
+window.title("Factoring Trinomials")
+
+eoutput1=StringVar()
+
+
+def clickFunction():
+    b=e2.get()
+    c=e3.get()
+
+    a=1
+    
+    b=int(b)
+    c=int(c)
+
+    answer1=((-b)+(math.sqrt((b**2)-(4*a*c))))/(2*a)
+    answer2=((-b)-(math.sqrt((b**2)-(4*a*c))))/(2*a)
+    if (answer1<0 and answer2<0):
+        answer1=str(answer1)
+        answer2=str(answer2)   
+        result=("(x"+answer1+")(x"+answer2+")")
+
+    elif (answer1>0 and answer2>0):
+        answer1=str(answer1)
+        answer2=str(answer2) 
+        result=("(x+"+answer1+")(x+"+answer2+")")
+
+    elif (answer1<0 and answer2>0):
+        answer1=str(answer1)
+        answer2=str(answer2) 
+        result=("(x"+answer1+")(x+"+answer2+")")
+
+    elif (answer1>0 and answer2<0):
+        answer1=str(answer1)
+        answer2=str(answer2) 
+        result=("(x+"+answer1+")(x"+answer2+")")
+
+    entry1.delete(0, END)
+    entry1.insert(0,result)
+   
+
+l1=Label(window, text='Instructions: Enter a value for a, b, and c in the boxes below,\nthen click the "Factor Trinomial" button to display the answer')
+e1=Label(window, text="1", width=2)
+e2=Entry(window, width=2)
+e3=Entry(window, width=2)
+l2=Label(window, text="a:")
+l3=Label(window, text="b:")
+l4=Label(window, text="c:")
+
+b1=Button(window, text="Factor Trinomial", command=clickFunction)
+
+entry1 = Entry(window, width=12, textvariable=eoutput1)
+
+l1.grid(row=1, column=1)
+e1.grid(row=3, column=1, sticky=W)
+e2.grid(row=4, column=1, sticky=W)
+e3.grid(row=5, column=1, sticky=W)
+l2.grid(row=3, column=0)
+l3.grid(row=4, column=0)
+l4.grid(row=5, column=0)
+b1.grid(row=4, column=1)
+entry1.grid(row=5, column=1)
+
+mainloop()
